3be6c13249a11b6a980ef2caa1ca9cffad50c105
Darrow Palast <dpalast@gmail.com>
2019-08-02 20:03:57 +0000

    Done.

--- final_hash.rb
+++ final_hash.rb
@@ -12,24 +12,13 @@ monopoly =  {
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
+    
+    :names=>{
+      
+      :reading_railroad=> {"mortgage_value"=>"$100"},
+      :pennsylvania_railroad=> {"mortgage_value"=>"$200"},
+      :b_and_o_railroad=> {"mortgage_value"=>"$400"},
+      :shortline=> {"mortgage_value"=>"$800"}
     }
   }
 }
